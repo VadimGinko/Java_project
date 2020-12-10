c7a41ab237b1590511c203644155a2530cbdfa2d
VadimGinko <ghinko2001@gmail.com>
2020-12-10 23:40:40 +0300

    student panel complete + pagination

--- src/main/java/com/example/students/repositories/IPerfomanceRepository.java
+++ src/main/java/com/example/students/repositories/IPerfomanceRepository.java
@@ -3,10 +3,14 @@ package com.example.students.repositories;
 import com.example.students.model.AcademicPerformance;
 import com.example.students.model.Subject;
 import com.example.students.model.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import java.util.List;
 
 public interface IPerfomanceRepository  extends JpaRepository<AcademicPerformance, Integer>  {
     <S extends AcademicPerformance> S save(S s);
     List<AcademicPerformance> findByUser(User user);
+    Page<AcademicPerformance> findByUserAndSubject(User user, Subject subject, Pageable pageable);
 }
--- src/main/java/com/example/students/rest/StudentRestControllerV1.java
+++ src/main/java/com/example/students/rest/StudentRestControllerV1.java
@@ -1,28 +1,45 @@
 package com.example.students.rest;
 
 import com.example.students.dto.StudentDto;
+import com.example.students.model.AcademicPerformance;
+import com.example.students.model.Role;
+import com.example.students.model.Subject;
 import com.example.students.model.User;
+import com.example.students.repositories.IPerfomanceRepository;
+import com.example.students.repositories.ISubjectRepository;
+import com.example.students.repositories.IUserRepository;
 import com.example.students.services.PerfomanceService;
-import com.example.students.services.SubjectService;
 import com.example.students.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/api/v1/students/")
 public class StudentRestControllerV1 {
     private final UserService userService;
     private final PerfomanceService perfomanceService;
-
+    private final IPerfomanceRepository iPerfomanceRepository;
+    private final ISubjectRepository iSubjectRepository;
+    private final IUserRepository iUserRepository;
     @Autowired
-    public StudentRestControllerV1(UserService userService, PerfomanceService perfomanceService) {
+    public StudentRestControllerV1(UserService userService, PerfomanceService perfomanceService, IPerfomanceRepository iPerfomanceRepository, ISubjectRepository iSubjectRepository, IUserRepository iUserRepository) {
         this.userService = userService;
         this.perfomanceService = perfomanceService;
+        this.iPerfomanceRepository = iPerfomanceRepository;
+        this.iSubjectRepository = iSubjectRepository;
+        this.iUserRepository = iUserRepository;
     }
 
     @GetMapping(value = "{username}")
@@ -42,5 +59,22 @@ public class StudentRestControllerV1 {
     public ResponseEntity<List<String>> getStudentByUsername(@RequestParam Map<String, String> mapParam){
         var username = mapParam.get("username");
         return new ResponseEntity<>(perfomanceService.findByUser(username), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "getUserMarks")
+    public Page<AcademicPerformance> getStudentMarks(@RequestParam Map<String, String> mapParam ){
+        var username = mapParam.get("username");
+        var subjectName = mapParam.get("subject");
+        System.out.println(mapParam.get("page"));
+        Optional<Integer> page = Optional.empty();
+        if(mapParam.get("page") != null)
+           page = Optional.of(Integer.parseInt(mapParam.get("page")));
+        User user1 = iUserRepository.findByUsername(username);
+        Subject subject = iSubjectRepository.findBySubject(subjectName).get(0);
+        var q = iPerfomanceRepository.findByUserAndSubject(user1, subject,  PageRequest.of(page.orElse(0),2));
+        for(var i:q){
+            i.getUser().setRoles(List.of(new Role()));
+        }
+        return q;
     }
 }
