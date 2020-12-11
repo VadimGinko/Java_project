fd10c79ffb85c3f8274daab185d22793c07cb1da
VadimGinko <54863832+VadimGinko@users.noreply.github.com>
2020-12-11 16:27:45 +0300

    Update MyConstants.java
    last commit

--- src/main/java/com/example/students/mail/MyConstants.java
+++ src/main/java/com/example/students/mail/MyConstants.java
@@ -4,7 +4,7 @@ public class MyConstants {
 
     public static final String MY_EMAIL = "ghinko2001@gmail.com";
 
-    public static final String MY_PASSWORD = "67883933073678FASTER";
+    public static final String MY_PASSWORD = "pass";
 
     public static final String TO_EMAIL = "ghinko@mail.ru";
 
